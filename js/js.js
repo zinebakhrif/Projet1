85749e65c3fe0274c9c561a4a3fea24216fed08f
zinebakhrif <78706709+zinebakhrif@users.noreply.github.com>
2021-04-22 22:41:20 +0000

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,20 +1,57 @@
+
+
 //    Lien 1 
 $(document).ready(function(){
     $("#l1,#l2").click(function(){
+      $("contentform").css("display", "block");
+      $(".divtable").css("display", "none");
       $(".container").css("width", "80%");
-      $("#input2").css("border-right", "2px solid #a8a383");
       $("#contform2").css("display", "none");
         $("#contform1").css("display", "flex");
         $(".flex-container").css("flex", "100%");
+      $(".header .navbar:last-child").css("z-index", "1");     
+
     });
   });
 //    Lien 2 
   $(document).ready(function(){
     $("#l3,#l4").click(function(){
+      $("contentform").css("display", "block");
+      $(".divtable").css("display", "none");
       $("#contform1").css("display", "none");
         $("#contform2").css("display", "flex");
-        $("#input2").css("border-right", "none");
         $(".container").css("width", "70%");
+      $(".header .navbar:last-child").css("z-index", "1");     
+
     });
   });
+
+  $(document).ready(function(){
+    $("#btn1").click(function(){
+      $(".header .navbar:last-child").css("z-index", "2");     
+      $(".divtable").css("transition", "max-height 0.6s, padding 0.6s");
+      $(".divtable").css("display", "block");
+    });
+  });
+  $(document).ready(function(){
+    $("#btn2").click(function(){
+      $("contentform").css("display", "block");
+      $("#contform1").css("display", "none");
+      $(".header .navbar:last-child").css("z-index", "2");     
+      $(".divtable").css("transition", "max-height 0.6s, padding 0.6s");
+      $(".divtable").css("display", "block");
+      $(".flex-item-left").css("display", "block");
+      $(".flex-item-right").css("display", "none");
+    });
+  });
+
+  // $(document).ready(function(){
+  //   $("#btn2").click(function(){
+  //     $(".divtable").css("transition", "max-height 0.6s, padding 0.6s");
+  //     $(".flex-item-left").css("display", "block");
+  //     $(".flex-item-right").css("display", "none");
+  //     $("#contform1").css("display", "none");
+  //     $("#contform2").css("display", "flex");
+  //   });
+  // });
 
