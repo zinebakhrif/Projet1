7eea3a84a8f36b50fd356291f83dcf7abd246d84
zinebakhrif <78706709+zinebakhrif@users.noreply.github.com>
2021-04-23 23:42:47 +0000

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,11 +1,23 @@
 
+    $(document).ready(function(){
+      $(".menu1 > li:not(.lien6,.lien5) a").click(function(){
+      $(".menu1 > li:not(.lien6,.lien5) a").css("color", "#f4f4f4");
+        $(this).css("color", "#f8d001"); 
+      });
+      });
+      $(document).ready(function(){
+        $(".menu2 > li a").click(function(){
+        $(".menu2 > li a").css("color", "#f4f4f4");
+          $(this).css("color", "#f8d001"); 
+        });
+        });
+
 
 //    Lien 1 
 $(document).ready(function(){
     $("#l1,#l2").click(function(){
-      $("contentform").css("display", "block");
       $(".divtable").css("display", "none");
-      $(".container").css("width", "80%");
+      $(".container").css("width", "77%");
       $("#contform2").css("display", "none");
         $("#contform1").css("display", "flex");
         $(".flex-container").css("flex", "100%");
@@ -13,14 +25,14 @@ $(document).ready(function(){
 
     });
   });
+
 //    Lien 2 
   $(document).ready(function(){
     $("#l3,#l4").click(function(){
-      $("contentform").css("display", "block");
       $(".divtable").css("display", "none");
       $("#contform1").css("display", "none");
         $("#contform2").css("display", "flex");
-        $(".container").css("width", "70%");
+        $(".container").css("width", "77%");
       $(".header .navbar:last-child").css("z-index", "1");     
 
     });
@@ -35,7 +47,6 @@ $(document).ready(function(){
   });
   $(document).ready(function(){
     $("#btn2").click(function(){
-      $("contentform").css("display", "block");
       $("#contform1").css("display", "none");
       $(".header .navbar:last-child").css("z-index", "2");     
       $(".divtable").css("transition", "max-height 0.6s, padding 0.6s");
@@ -45,13 +56,9 @@ $(document).ready(function(){
     });
   });
 
-  // $(document).ready(function(){
-  //   $("#btn2").click(function(){
-  //     $(".divtable").css("transition", "max-height 0.6s, padding 0.6s");
-  //     $(".flex-item-left").css("display", "block");
-  //     $(".flex-item-right").css("display", "none");
-  //     $("#contform1").css("display", "none");
-  //     $("#contform2").css("display", "flex");
-  //   });
-  // });
+
+
+
+
+
 
