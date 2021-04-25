f3972c4fe7c40b4a4d675eec359e70d8d36b5fca
zinebakhrif <78706709+zinebakhrif@users.noreply.github.com>
2021-04-25 01:18:29 +0000

    Update js.js

--- js/js.js
+++ js/js.js
@@ -12,16 +12,16 @@
         });
         });
 
-
 //    Lien 1 
 $(document).ready(function(){
     $("#l1,#l2").click(function(){
       $(".divtable").css("display", "none");
+      $("#lblerreur").css("display", "none");
       $(".container").css("width", "77%");
       $("#contform2").css("display", "none");
         $("#contform1").css("display", "flex");
         $(".flex-container").css("flex", "100%");
-      $(".header .navbar:last-child").css("z-index", "1");     
+
 
     });
   });
@@ -29,33 +29,35 @@ $(document).ready(function(){
 //    Lien 2 
   $(document).ready(function(){
     $("#l3,#l4").click(function(){
+      $("#lblerreur").css("display", "none");
       $(".divtable").css("display", "none");
       $("#contform1").css("display", "none");
         $("#contform2").css("display", "flex");
         $(".container").css("width", "77%");
-      $(".header .navbar:last-child").css("z-index", "1");     
 
     });
   });
 
-  $(document).ready(function(){
-    $("#btn1").click(function(){
-      $(".header .navbar:last-child").css("z-index", "2");     
-      $(".divtable").css("transition", "max-height 0.6s, padding 0.6s");
-      $(".divtable").css("display", "block");
-    });
-  });
-  $(document).ready(function(){
-    $("#btn2").click(function(){
-      $("#contform1").css("display", "none");
-      $(".header .navbar:last-child").css("z-index", "2");     
-      $(".divtable").css("transition", "max-height 0.6s, padding 0.6s");
-      $(".divtable").css("display", "block");
-      $(".flex-item-left").css("display", "block");
-      $(".flex-item-right").css("display", "none");
-    });
-  });
+ 
 
+  function myshow(){
+    var txt1=document.getElementById("input1").value;
+    var txt2=document.getElementById("input2").value;
+    var txt3=document.getElementById("input3").value;
+    var txt4=document.getElementById("input4").value;
+ 
+    if(txt1!='' && txt2!='' && txt3!='' && txt4!=''){
+      document.getElementById("frm").style.display="block";
+      document.getElementById("lblerreur").style.display="none";
+
+    }else{
+      document.getElementById("lblerreur").style.display="block";
+    }
+  }
+  
+
+
+ 
 
 
 
